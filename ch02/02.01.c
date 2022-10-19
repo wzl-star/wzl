5d6607349d0d1c49ba4bcae24fd26392b50328b8
wzl-star <116146939+wzl-star@users.noreply.github.com>
2022-10-19 20:45:53 +0800

    23
    12

---
+++ ch02/02.01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    num = 1;
+
+    printf("i am a simple");
+    printf("computer.\n");
+    printf("My favorite numeber is %d because it is first.\n", num);
+
+    return 0;
+}
